db75b39f3be426495bca5956508a641adea0017c
6ixtoes <email.abdulzhamman24@gmail.com>
2025-06-12 02:14:39 -1100

    added code to copy zip file to repo and push

--- Project1.py
+++ Project1.py
@@ -23,8 +23,17 @@ def setup_git_repo(local_repo_path, github_url):
         subprocess.run(["git", "clone", github_url, local_repo_path], check=True)
     else:
         print("[+] Local repo exists Using it.")
+def push_to_github(repo_path, zip_file_path, commit_message="Auto-commit zip file"):
+    shutil.copy(zip_file_path, repo_path)
+    os.chdir(repo_path)
 
 
+    subprocess.run(["git", "add", "."], check=True)
+    subprocess.run(["git", "commit", "-m", commit_message], check=True)
+    subprocess.run(["git", "push"], check=True)
+    
+    print("[+] Pushed to Github successfully.")
+    
 
 
 
